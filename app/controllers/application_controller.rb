184e186bca2fb051920a382cf59eff1e2e3381a3
黒田 <ddki313kr2@gmail.com>
2026-06-23 23:41:27 +0900

    feat: ログイン後のリダイレクト先設定 #49

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :authenticate_user!
+
+  private
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 end
