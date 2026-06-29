c30678079689222ab6eec0f6920f66cfb1e0dd04
黒田 <ddki313kr2@gmail.com>
2026-06-29 20:24:38 +0900

    feat: グループ作成ページ・機能 #11 #12

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,25 @@ class GroupsController < ApplicationController
   def index
     @groups = current_user.groups
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = current_user.owned_groups.build(group_params)
+
+    if @group.save
+      @group.group_memberships.create!(user: current_user, role: :admin)
+      redirect_to groups_path, notice: t(".success")
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :photo_limit)
+  end
 end
