7cc35e15d779002238d7e5a2a933dd3d1eff6374
黒田 <ddki313kr2@gmail.com>
2026-06-30 11:51:07 +0900

    feat: 現像待ち機能（visible_atの設定ロジック） #25

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,23 @@
 class Photo < ApplicationRecord
+  DEVELOPING_PERIOD = 24.hours
+
   belongs_to :user
   belongs_to :group
 
   has_one_attached :image
+
+  scope :visible, -> { where("visible_at <= ?", Time.current) }
+  scope :developing, -> { where("visible_at > ?", Time.current) }
+
+  before_validation :set_visible_at, on: :create
+
+  def developed?
+    visible_at.present? && visible_at <= Time.current
+  end
+
+  private
+
+  def set_visible_at
+    self.visible_at ||= (taken_at || Time.current) + DEVELOPING_PERIOD
+  end
 end
