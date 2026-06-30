1e4d2f45cb60c3d53573195b623c839241bd0dfc
黒田 <ddki313kr2@gmail.com>
2026-06-30 11:41:49 +0900

    feat: カメラ撮影ページ・ブラウザカメラAPI連携 #21 #22

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,34 @@
+class PhotosController < ApplicationController
+  before_action :set_group
+
+  def new
+    @remaining = remaining_photos
+  end
+
+  def create
+    if remaining_photos <= 0
+      redirect_to new_group_photo_path(@group), alert: t(".limit_reached")
+      return
+    end
+
+    @photo = @group.photos.build(user: current_user, taken_at: Time.current)
+    @photo.image.attach(params[:image])
+
+    if @photo.save
+      redirect_to new_group_photo_path(@group), notice: t(".success")
+    else
+      redirect_to new_group_photo_path(@group), alert: t(".failure")
+    end
+  end
+
+  private
+
+  def set_group
+    @group = current_user.groups.find(params[:group_id])
+  end
+
+  def remaining_photos
+    return Float::INFINITY unless @group.photo_limit
+    @group.photo_limit - @group.photos.count
+  end
+end
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import CameraController from "./camera_controller"
+application.register("camera", CameraController)
