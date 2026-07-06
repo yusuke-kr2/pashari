4536015784e9ad21b88bb02c22ceb9d2bb64b1b4
黒田 <ddki313kr2@gmail.com>
2026-07-06 21:23:29 +0900

    feat: ローディングアニメーション設定 #57

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,8 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+// プログレスバーの表示遅延を短くする
+Turbo.setProgressBarDelay(200)
